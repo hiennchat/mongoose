0191ecb6c5bbe8ac3c9b28b99b59e9f990329a32
Hien Nguyen <hiennc@zen-s.com>
2022-06-29 15:56:44 +0700

    remove usecreateindex vs usefindandmodify for mongoose version ^6

--- src/database.ts
+++ src/database.ts
@@ -33,9 +33,7 @@ export class MongoDB {
       user: this.config.user,
       pass: this.config.password,
       useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false
+      useNewUrlParser: true
     });
   }
 
